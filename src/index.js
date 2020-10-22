b9a42409d1d10933b7aa748e8fb48ba151d0fbf7
priyakwatra <pkwatra96@gmail.com>
2020-10-22 12:36:53 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import Description from './components/description/Description';
+import Cart from './components/cart/Cart';
 import './index.css';
-import App from './App';
+import Inventory from './components/inventory/Inventory';
+import Navigation from './components/navigation/Navigation';
+import Login from './components/login/Login';
+import rootReducer from './reducers/index';
 import * as serviceWorker from './serviceWorker';
+import { withRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(<Provider store={store}>
+  <Router >
+    <Navigation />
+    <Switch>
+      <Route exact component={Inventory} path="/" />
+      <Route exact path="/description/:productName" component={Description} />
+      <Route exact path="/cart" component={Cart} />
+      <Route exact component={Login} path="/login" />
+    </Switch>
+  </Router>
+</Provider>,
   document.getElementById('root')
 );
 
